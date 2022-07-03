46d1b23c07a7eca27145d6f73b333715c558a697
isa yeter <3602109+isayeter@users.noreply.github.com>
2022-07-03 19:24:36 +0300

    Update to swift 4.2

--- InAppNotify/NotificationFactory.swift
+++ InAppNotify/NotificationFactory.swift
@@ -97,8 +97,8 @@ open class NotificationFactory: UIView,UITextViewDelegate {
         let button       = UIButton()
         button.isEnabled = false
         
-        button.setTitle(InAppNotify.sendString, for: UIControlState())
-        button.setTitleColor(InAppNotify.theme.sendButtonNormalColor, for: UIControlState())
+        button.setTitle(InAppNotify.sendString, for: UIControl.State())
+        button.setTitleColor(InAppNotify.theme.sendButtonNormalColor, for: UIControl.State())
         button.setTitleColor(InAppNotify.theme.sendButtonHighlightedColor, for: .highlighted)
         return button
     }()
@@ -199,7 +199,7 @@ open class NotificationFactory: UIView,UITextViewDelegate {
         buttonSend.addTarget(self, action:#selector(self.interactionSend), for: .touchUpInside)
         
         //Register for rotation change
-        NotificationCenter.default.addObserver(self, selector: #selector(self.orientationDidChange), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.orientationDidChange), name: UIDevice.orientationDidChangeNotification, object: nil)
     }
     
     public required init?(coder aDecoder: NSCoder) {
@@ -208,7 +208,7 @@ open class NotificationFactory: UIView,UITextViewDelegate {
     
     deinit {
         //Deregister for rotation change
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIDevice.orientationDidChangeNotification, object: nil)
     }
     
     
@@ -231,7 +231,7 @@ open class NotificationFactory: UIView,UITextViewDelegate {
         textView.frame              = newFrame;
         buttonSend.isEnabled        = textView.text != ""
         buttonSend.frame.origin.y   = inputText.frame.origin.y+inputText.frame.size.height-buttonSend.frame.size.height
-        let stringLength:Int        = textView.text.characters.count
+        let stringLength:Int        = textView.text.count
         
         textView.scrollRangeToVisible(NSMakeRange(stringLength-1, 0))
         
